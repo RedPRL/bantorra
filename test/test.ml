65e82f68ed22ce19d778d527fb8e1504dbbc1e0f
favonia <favonia@gmail.com>
2020-06-21 12:03:46 -0500

    Improve testing for Cache.Database.

--- test/test.ml
+++ test/test.ml
@@ -2,15 +2,20 @@ open Cache
 let () = print_endline "Testing the database located at /tmp/bantorra/testing"
 let db = Database.init ~root:"/tmp/bantorra/testing"
 let () = print_endline "Adding an item..."
-let digest = Database.replace_item db ~key:(`Null) ~value:(`A [])
+let digest = Database.replace_item db ~key:`Null ~value:(`A [])
 let () = print_endline "Finding an item without checking its digest..."
 let () =
-  match Database.find_item_opt db ~key:(`Null) ~digest:None with
+  match Database.find_item_opt db ~key:`Null ~digest:None with
+  | Some `A [] -> ()
+  | _ -> assert false
+let () = print_endline "Finding an item with the correct digest..."
+let () =
+  match Database.find_item_opt db ~key:`Null ~digest:(Some digest)  with
   | Some `A [] -> ()
   | _ -> assert false
 let () = print_endline "Finding an item with a wrong digest..."
 let () =
-  match Database.find_item_opt db ~key:(`Null) ~digest:(Some "123") with
+  match Database.find_item_opt db ~key:`Null ~digest:(Some "123") with
   | None -> ()
   | _ -> assert false
 let () = print_endline "Saving the database meta information..."
