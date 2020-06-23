cd952e13f499684b9c0ba05ffdcc0c3517cef35c
favonia <favonia@gmail.com>
2020-06-22 20:45:43 -0500

    Re-organize the code a bit.

--- src/manager/Config.ml
+++ src/manager/Config.ml
@@ -9,24 +9,27 @@ type t = {libraries : (lib_name, string) Hashtbl.t}
 
 let default = {libraries = Hashtbl.create 0}
 
-let to_library : Marshal.value -> lib_name * string =
-  function
-  | `O ["name", name; "version", version; "root", root] ->
-    {name = Marshal.to_string name; version = Marshal.to_ostring version},
-    Marshal.to_string root
-  | _ -> raise Marshal.IllFormed
+module M =
+struct
+  let to_library : Marshal.value -> lib_name * string =
+    function
+    | `O ["name", name; "version", version; "root", root] ->
+      {name = Marshal.to_string name; version = Marshal.to_ostring version},
+      Marshal.to_string root
+    | _ -> raise Marshal.IllFormed
 
-let of_library (({name; version} : lib_name), root) =
-  `O ["name", Marshal.of_string name; "version", Marshal.of_ostring version; "root", Marshal.of_string root]
+  let of_library (({name; version} : lib_name), root) =
+    `O ["name", Marshal.of_string name; "version", Marshal.of_ostring version; "root", Marshal.of_string root]
+end
 
 let deserialize : Marshal.value -> t =
   function
   | `O ["format", `String v; "libraries", `A libs] when v = version ->
-    {libraries = Hashtbl.of_seq @@ Seq.map to_library @@ List.to_seq libs}
+    {libraries = Hashtbl.of_seq @@ Seq.map M.to_library @@ List.to_seq libs}
   | _ -> raise Marshal.IllFormed
 
 let serialize ({libraries} : t) : Marshal.value =
-  let libraries = List.of_seq @@ Seq.map of_library @@ Hashtbl.to_seq libraries in
+  let libraries = List.of_seq @@ Seq.map M.of_library @@ Hashtbl.to_seq libraries in
   `O ["format", `String version; "libraries", `A libraries]
 
 let config_filepath ~app_name =
