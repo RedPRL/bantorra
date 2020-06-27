4ab73351d1b4060d9408d05c6ecaa7fe93b026e9
favonia <favonia@gmail.com>
2020-06-27 10:25:36 -0500

    Update the comments for BantorraResolvers.OCamlFind.

--- src/resolvers/OCamlFind.mli
+++ src/resolvers/OCamlFind.mli
@@ -1,7 +1,7 @@
 
 (** {1 Introduction}
 
-    This is a resolver similar to the fixed-table resolver {!module:FixedTable} except that each path is relative the package directory given by the [ocamlfind] tool.
+    This is a resolver similar to the fixed-table resolver {!module:FixedTable} except that each path is relative the package directory given by the OCaml library {{:http://projects.camlcity.org/projects/findlib.html}findlib}. The package directory is used by the [ocamlfind] tool as part of the standard OCaml toolchain.
 
     The resolver argument format is
     {[
@@ -13,4 +13,4 @@
 *)
 
 val resolver : package_name:string -> dict:(string * string) list -> Bantorra.Resolver.t
-(** [resolver ~package_name ~dict] constructs a resolver based on the package directory given by the [ocamlfind] tool and the mapping [dict]. All paths are normalized and turned into absolute paths with respect to the current working directory using {!val:BantorraBasis.File.normalize_dir}. *)
+(** [resolver ~package_name ~dict] constructs a resolver based on the package directory given by {{:http://projects.camlcity.org/projects/findlib.html}findlib} and the mapping [dict]. All paths are normalized and turned into absolute paths with respect to the current working directory using {!val:BantorraBasis.File.normalize_dir}. *)
