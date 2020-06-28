6ec6a315937f36aaaaa04a30fd6aa5948ee2a933
favonia <favonia@gmail.com>
2020-06-27 22:01:26 -0500

    Tweaks.

--- src/basis/Exec.mli
+++ src/basis/Exec.mli
@@ -1,5 +1,5 @@
 val system : prog:string -> args:string list -> unit
-(** A more usable UnixLabels.system. *)
+(** A more structured {!val:UnixLabels.system}. *)
 
 val with_system_in : prog:string -> args:string list -> (in_channel -> 'a) -> 'a
-(** A high-level UnixLabels.open_process_in that will automatically close the channel. *)
+(** A higher-level interface to {!val:UnixLabels.open_process_in} that will automatically close the channel. *)
