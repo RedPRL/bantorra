ecb5d2e1a34336b4b6657f200057097c6f60deed
favonia <favonia@gmail.com>
2020-06-28 19:06:18 -0500

    Documentation on anchors.

--- src/manager/Anchor.mli
+++ src/manager/Anchor.mli
@@ -9,13 +9,31 @@ format: "1.0.0"
     {v
 format: "1.0.0"
 deps:
-  - mount_point: [lib, num]
+  - mount_point: [path, to, lib1]
+    resolver: solver
+    res_args:
+      ...
+  - mount_point: [path, to, lib2]
     resolver: solver
     res_args:
       ...
     v}
 
     If the [deps] field is missing or the entire anchor is empty (or equivalent to the YAML value [null]), then it means the library has no dependency. Each dependency is specifed by its mount point ([mount_point]), the name of the resolver ([resolver]), and the arguments to the resolver ([res_args]). During the resolution, the entire YAML subtree under ([res_args]) is sent to the resolver. See {!type:Resolver.res_args}.
+
+    The order of entries in [dep] does not matter and the dispatching is based on longest prefix match. The same library can have multiple mount points. To keep things unambiguous, there cannot be two entries sharing the same mount point, and the mount point cannot be an empty list (the root). Here is an example showing the longest prefix match:
+    {v
+format: "1.0.0"
+deps:
+  - mount_point: [tcp]
+    resolver: builtin
+    res_args: tcp
+  - mount_point: [tcp, http]
+    resolver: builtin
+    res_args: http
+    v}
+
+    The unit path [tcp.ftp] will be resolved to [ftp] in the [tcp] library, awaiting further resolution, while the unit path [tcp.http.connect] will be resolved to [connect] in the [http] library, not [http.connect] in the [tcp] library. Again, the order of dependencies does not matter.
 *)
 
 (** {1 Types} *)
@@ -44,6 +62,4 @@ val iter_deps : (lib_ref -> unit) -> t -> unit
 (** [iter_lib_refs f a] runs [f] on each dependency listed in the anchor [a]. *)
 
 val dispatch_path : t -> unitpath -> (lib_ref * unitpath) option
-(** [dispatch_path a p] resolves the unit path [p] to [Some (ref, p')] if it points to
-    another unit in another library referenced by [ref] and [p'],
-    or [None] if it is a local unit path. *)
+(** [dispatch_path a p] resolves the unit path [p] to [Some (ref, p')] if it points to a unit in another library referenced by [ref] and [p'], or [None] if it is a local unit path. The dispatching is done by longest prefix match. *)
