dc3d4ae9b5c1f99ed3a1f30fcccb2118163ff5f8
favonia <favonia@gmail.com>
2020-06-29 08:53:02 -0500

    Fix locate_anchor.

--- src/basis/File.mli
+++ src/basis/File.mli
@@ -44,18 +44,25 @@ val normalize_dir : string -> string
    The current working directory will be restored after the computation.
 *)
 
+val parent_of_normalized_dir : string -> string option
+(**
+   [parent_of_normalized_dir dir] calculates the parent of a normalized directory [dir]. If [dir] is already the root directory, then this function returns [None]; otherwise it returns [Some parent] where [parent] is the parent directory. The result could be wrong if [dir] was not already normalized.
+*)
+
 (** {1 Locating Files} *)
 
 val locate_anchor : anchor:string -> string -> string * string list
 (**
-   [locate_anchor ~anchor dir] finds the closest regular file named [anchor] in [dir] or its ancestors
-    in the file system tree.
-    It returns the first directory that holds the file named [anchor] on the way from [dir] to the root directory,
-    along with the relative path from the returned directory to [dir].
-    The exception [Not_found] is raised if such a file cannot be found.
+   [locate_anchor ~anchor dir] finds the closest regular file named [anchor] in [dir] or its ancestors in the file system tree.
 
-    For example, on a typical Linux system, suppose there is no file called [anchor.txt] under directiors
-    [/usr/lib/gcc/] and [/usr/lib/], but there is such a file under [/usr/].
-    [locate_anchor ~anchor:"anchor.txt" "/usr/lib/gcc"] will return ["/usr", ["lib"; "gcc"]]
-    and [locate_anchor ~anchor:"anchor.txt" "/usr"] will return ["/usr", []].
+   @param dir The starting directory. It will be normalized with respect to the current working director.
+
+   @return
+   (1) the first directory that holds a regular file named [anchor] on the way from [dir] to the root directory; and (2) the relative path from the returned directory to [dir].
+   The exception [Not_found] is raised if such a file cannot be found.
+
+   For example, on a typical Linux system, suppose there is no file called [anchor.txt] under directiors
+   [/usr/lib/gcc/] and [/usr/lib/], but there is such a file under [/usr/].
+   [locate_anchor ~anchor:"anchor.txt" "/usr/lib/gcc"] will return ["/usr", ["lib"; "gcc"]]
+   and [locate_anchor ~anchor:"anchor.txt" "/usr"] will return ["/usr", []].
 *)
