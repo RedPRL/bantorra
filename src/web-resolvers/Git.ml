a516f4cd0e8616301e5651bbe12b50126cc59b2a
favonia <favonia@gmail.com>
2020-07-01 09:49:54 -0500

    git reset really hard! (#25)

--- src/web-resolvers/Git.ml
+++ src/web-resolvers/Git.ml
@@ -34,7 +34,7 @@ struct
         Exec.system ~prog:"git" ~args:["fetch"; "--quiet"; "--no-tags"; "--recurse-submodules=on-demand"; "--depth=1"; "--"; url; ref];
         match id_in_use with
         | None ->
-          Exec.system ~prog:"git" ~args:["reset"; "--quiet"; "--hard"; "--"; "FETCH_HEAD"];
+          Exec.system ~prog:"git" ~args:["reset"; "--quiet"; "--hard"; "--recurse-submodules"; "--"; "FETCH_HEAD"];
           Exec.with_system_in ~prog:"git" ~args:["rev-parse"; "HEAD"] @@ fun ic_id ->
           String.trim @@ input_line ic_id
         | Some id_in_use ->
