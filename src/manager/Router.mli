b76a70025807238bb93c139f97df0bcc9927d37c
favonia <favonia@gmail.com>
2021-07-05 14:41:48 -0500

    Tweaks.

--- src/manager/Router.mli
+++ src/manager/Router.mli
@@ -16,7 +16,7 @@ val make :
   t
 (** [make ?fast_checker route] creates a new router that can be used in {!val:Manager.init}.
 
-    @param route [route ~starting_dir ~arg] is responsible for finding the root of the library specified by [arg]. A library manager will feed the router with unparsed [router_argument] field in an anchor file. (See {!module:Manager}.)
+    @param route [route ~starting_dir ~arg] is responsible for finding the root of the library specified by [arg]. A library manager will feed the router with unparsed [router_argument] field in an anchor file as [arg] and the root of the mounting library as [starting_dir]. (See {!module:Manager}.)
     @param fast_checker A validity checker for dependencies in anchor files when loading a library (before the units from other libraries are actually needed). It is taking the same arguments as the [router] does, but it only needs to check whether the resolution could have been successful. Some library resolution is expensive (e.g., involving downloading the sources from the server) and a faster, incomplete validity checker might be desirable. If absent, [route] will be used as the checker.
 *)
 
