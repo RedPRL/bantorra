e48b90ed649d8d102a2e04e4c33fdec71653720e
favonia <favonia@gmail.com>
2022-09-29 23:10:13 -0400

    fix: add lookup_table and rename ConfigFile to Table

--- src/internal/ConfigFile.mli
+++
@@ -1,7 +0,0 @@
-val parse : version:string -> string -> (Marshal.value, Marshal.value) Hashtbl.t
-
-val read : version:string -> FilePath.t -> (Marshal.value, Marshal.value) Hashtbl.t
-
-val get_web : version:string -> string -> (Marshal.value, Marshal.value) Hashtbl.t
-
-val write : version:string -> FilePath.t -> (Marshal.value, Marshal.value) Hashtbl.t -> unit
